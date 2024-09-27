95e53604445f247a2017f892ef2fb9b31a73f884
Farhana Alam Japse <farhanjapz@gmail.com>
2024-09-27 21:44:29 +0600

    scrolling

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 'use strict';
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect(); //DOMRect {x: 0, y: 119, width: 631, height: 2441.1875, top: 119, …}
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect()); //DOMRect {x: 30, y: 262.78125, width: 110, height: 29, top: 262.78125, …}
+
+  //console.log('current scroll(X/Y)', window.pageXOffset, window.pageYOffsets);
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+  //scrolling
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
 ///////////////////////////////////////
 // Modal window
 
@@ -8,7 +24,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -17,9 +34,10 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+btnsOpenModal.forEach(btn => btn.addEventListener('click', closeModal));
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
